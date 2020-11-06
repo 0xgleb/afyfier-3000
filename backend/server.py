f45734198cce4028ae4b13c256710c3c80c5bcae
Lefteris Kritikos <lefteris.kritikos@habito.com>
2020-11-06 12:52:36 +0000

    Add CORS

--- backend/server.py
+++ backend/server.py
@@ -2,6 +2,13 @@ from simplifier import afyfy
 from flask import Flask, request
 app = Flask(__name__)
 
+@app.after_request
+def after_request(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Headers", "*")
+    response.headers.add("Access-Control-Allow-Methods", "*")
+    return response
+
 @app.route('/afyfy', methods=['POST'])
 def hello_world():
     return afyfy(request.form['text'])
